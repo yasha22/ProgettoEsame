274e4d10070350c3002e3120c31bae2ca8cbeb15
yasser <yassercharef@outlook.com>
2018-01-30 11:14:02 +0100

    Create login page

---
+++ login.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> 
+  </head>
+
+  <body>
+	<nav class="navbar navbar-expand-sm bg-light navbar-light">
+		<a class="navbar-brand" href="#">Progetto 0.1a</a>
+		
+		<a class="mx-auto navbar-brand"></h1>
+		
+		<a class="navbar-brand navbar-right text-primary" href="#">Iscriviti</a>
+		<a class="navbar-brand navbar-right text-primary" href="#">Accedi</a>
+
+	</nav>
+
+  </body>
+
+</html>
