073479fb0cbad4dcb10e795ef10baa7955f82d5c
yasser <yassercharef@outlook.com>
2018-01-30 10:08:15 +0100

    index page creation
    index page with navbar made with bootstrap 4

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> 
+  </head>
+
+  <body>
+	<nav class="navbar navbar-expand-sm bg-light navbar-light">
+		<a class="navbar-brand" href="#">
+			<img src="https://cdn1.iconfinder.com/data/icons/freeline/32/home_house_real_estate-512.png" width="30" height="30">
+		</a>
+
+		
+		<a class="mx-auto navbar-brand">Progetto 0.1a</h1>
+		
+
+		<a class="navbar-brand navbar-right" href="#">
+			<img src="https://cdn1.iconfinder.com/data/icons/trycons/32/settings-512.png" width="30" height="30">
+		</a>
+		<a class="navbar-brand navbar-right" href="#">
+			<img src="https://png.icons8.com/metro/1600/exit.png" width="30" height="30">
+		</a>
+	
+	</nav>
+	
+  </body>
+
+</html>
